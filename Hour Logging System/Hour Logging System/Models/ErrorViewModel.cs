fefb76c3c26825fddc3b233bcd4ec09b98e4b466
lukejelse04 <luke-else@outlook.com>
2021-07-27 17:56:34 +0100

    Initial Project Upload

---
+++ Hour Logging System/Hour Logging System/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Hour_Logging_System.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
